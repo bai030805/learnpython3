e72cb235ceba4460c4b5f4b7a8ba7f837cb7cb4b
bai030805 <bai030805@163.com>
2017-12-25 23:46:30 +0800

    input and output section done

--- 00hello.py
+++ 00hello.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
 print('hello world!')
 print('test1', 'test2', 'test3')
 print(300)
 print(200+300)
 print('100+200=',100+200)
 name = input()
+print(name)
+
+resolution = 1024 * 768
+print(resolution)
